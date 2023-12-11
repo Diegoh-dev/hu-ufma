c7e4e2a85b3b7f01e42e059d667d713e7fc0e796
Diegoh-dev <diegoh.developer@gmail.com>
2023-12-11 19:59:27 -0300

    feat: iniciando estilo da tela de serviços.

--- app/servicos.tsx
+++ app/servicos.tsx
@@ -1,9 +1,27 @@
-import { Text } from "react-native";
-
-export default function Servicos(){
-  return(
-    <>
-    <Text>Serviços</Text>
-    </>
-  )
+import { Text, View } from "react-native";
+import { Ionicons } from '@expo/vector-icons';
+export default function Servicos() {
+  return (
+    <View className="flex-1 px-5 pt-2">
+      <View className="flex-row justify-between">
+        <View>
+          <Text className="text-purplePrimary font-bold text-xl">
+            Olá, Diego
+          </Text>
+          <Text className="text-purplePrimary font-bold text-base">
+            Prontuário : 14438232
+          </Text>
+        </View>
+        <View>
+          <Ionicons name="exit" size={30} color="#414396" />
+          <Text className="text-purplePrimary font-semibold text-sm">Sair</Text>
+        </View>
+      </View>
+      <View className="items-center mt-4">
+        <Text className="text-[30px] font-bold text-purplePrimary ">
+          Serviços
+        </Text>
+      </View>
+    </View>
+  );
 }
