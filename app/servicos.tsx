6038053357c9af26e3df03fdf052c9e75ef94017
Diegoh-dev <diegoh.developer@gmail.com>
2023-12-11 22:05:21 -0300

    feat: modulos.

--- app/servicos.tsx
+++ app/servicos.tsx
@@ -1,5 +1,9 @@
 import { Text, View } from "react-native";
 import { Ionicons } from '@expo/vector-icons';
+import { MaterialCommunityIcons } from '@expo/vector-icons';
+import { Foundation } from '@expo/vector-icons';
+import { FontAwesome5 } from '@expo/vector-icons';
+
 export default function Servicos() {
   return (
     <View className="flex-1 px-5 pt-2">
@@ -17,10 +21,47 @@ export default function Servicos() {
           <Text className="text-purplePrimary font-semibold text-sm">Sair</Text>
         </View>
       </View>
-      <View className="items-center mt-4">
+      <View className="items-center mt-6 justify-center">
         <Text className="text-[30px] font-bold text-purplePrimary ">
           Serviços
         </Text>
+
+      <View className="w-full flex-col gap-6 mt-2">
+        <View className="mt-4 flex-row w-full relative">
+          <View className="w-20 h-20 bg-purplePrimary rounded-[22px] items-center justify-center z-10">
+            <MaterialCommunityIcons name="doctor" size={30} color="#FFF" />
+          </View>
+          <View className="w-72 h-16 bg-purpleSeccondary rounded-r-full	items-center justify-center absolute top-4 left-10">
+            <Text className="text-colorWhite text-2xl font-bold ">
+              Consultas
+            </Text>
+          </View>
+        </View>
+
+        <View className="mt-4 flex-row w-full relative">
+          <View className="w-20 h-20 bg-purplePrimary rounded-[22px] items-center justify-center z-10">
+          <FontAwesome5 name="notes-medical" size={30} color="#fff" />
+          </View>
+          <View className="w-72 h-16 bg-purpleSeccondary rounded-r-full	items-center justify-center absolute top-4 left-10">
+            <Text className="text-colorWhite text-2xl font-bold ">
+              Exames
+            </Text>
+          </View>
+        </View>
+
+        <View className="mt-4 flex-row w-full relative">
+          <View className="w-20 h-20 bg-purplePrimary rounded-[22px] items-center justify-center z-10">
+          <Foundation name="info" size={30} color="#fff" />
+          </View>
+          <View className="w-72 h-16 bg-purpleSeccondary rounded-r-full	items-center justify-center absolute top-4 left-10">
+            <Text className="text-colorWhite text-2xl font-bold ">
+              Informações
+            </Text>
+          </View>
+        </View>
+
+      </View>
+    
       </View>
     </View>
   );
